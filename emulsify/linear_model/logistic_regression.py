3a05f52255171a4a43840d611a59195aa6b56a87
Brian <bbader.stat@gmail.com>
2020-01-04 11:11:37 -0500

    sklearn engine

--- emulsify/linear_model/logistic_regression.py
+++ emulsify/linear_model/logistic_regression.py
@@ -1,12 +1,11 @@
 """Wrappers for logistic regression implementations."""
 
 import numpy as np
-import pandas as pd
+import scipy.stats as stat
+import statsmodels.api as sm
 from sklearn.base import BaseEstimator
 from sklearn.linear_model import LogisticRegression as ScikitLR
 from statsmodels.api import Logit as StatsModelsLR  # change to MNLogit
-import scipy.stats as stat
-import statsmodels.api as sm
 
 from emulsify.utils import _get_column_names
 from emulsify.linear_model.base import BaseLinear
@@ -21,13 +20,28 @@ class LogisticRegression(BaseEstimator, BaseLinear):
         self.fit_intercept = fit_intercept
 
     def set_engine(self, engine='scikit-learn', **kwargs):
+        self.engine = engine
         # ToDo: add engine validation check
         # ToDo: set up environment (H20, spark, and probably vowpall-wabbit)
         self.model_kwargs = kwargs or {}
         if engine == 'statsmodels':
             self.model = None  # statsmodels api requires X, y in init step
         else:
-            self.model = ScikitLR(C=1/self.penalty, l1_ratio=self.mixture, **kwargs)
+            self._sklearn_engine()
+
+    def _sklearn_engine(self):
+        if self.penalty == 0.:
+            self.model_kwargs['penalty'] = 'none'
+        elif self.mixture == 1.:
+            self.model_kwargs['penalty'] = 'l1'
+        elif self.mixture == 0.:
+            self.model_kwargs['penalty'] = 'l2'
+        else:
+            self.model_kwargs['penalty'] = 'elasticnet'
+            self.model_kwargs['l1_ratio'] = self.mixture
+        if self.penalty != 0.:
+            self.model_kwargs['C'] = 1/self.penalty
+        self.model = ScikitLR(**self.model_kwargs)
 
     def _fit_case(self):
         fit_case = {
@@ -37,7 +51,6 @@ class LogisticRegression(BaseEstimator, BaseLinear):
         return fit_case
 
     def fit(self, X, y, sample_weight=None, **fit_params):
-        self._initialize(X, y)
         self.names_ = _get_column_names(X, self.fit_intercept)
         self._fit_case()[self.engine](X, y, sample_weight, **fit_params)
         self.z_scores_ = self.estimates_ / self.std_errors_
