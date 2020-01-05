551b79581e991026415e49a1539decdc3ff006c0
Brian <bbader.stat@gmail.com>
2020-01-05 18:57:02 -0500

    fix up base classes

--- emulsify/base.py
+++ emulsify/base.py
@@ -1,15 +1,29 @@
 """Base methods for all models."""
 
-from abc import ABC, abstractmethod
 from patsy import dmatrices
 
 
-class BaseMixin(ABC):
-    @abstractmethod
+class BaseMixin:
     def fit(self, X, y, **fit_params):
-        return
+        pass
 
     def fit_from_formula(self, formula, data, **fit_params):
         # ToDo: figure out how to handle other input types (numpy?)
         y, X = dmatrices(formula, data=data, return_type="dataframe")
         self.fit(X, y, **fit_params)
+
+    def set_engine(self, engine='sklearn', **kwargs):
+        self.engine = engine
+        # ToDo: add engine validation check
+        # ToDo: set up environment (H20, spark, and probably vowpall-wabbit)
+        self.model_kwargs = kwargs or {}
+        if engine == 'statsmodels':
+            self._statsmodels_engine()
+        else:
+            self._sklearn_engine()
+
+    def _statsmodels_engine(self):
+        raise ValueError("Not implemented for statsmodels API")
+
+    def _sklearn_engine(self):
+        raise ValueError("Not implemented for scikit-learn API")
--- emulsify/linear_model/base.py
+++ emulsify/linear_model/base.py
@@ -3,24 +3,10 @@
 import pandas as pd
 from sklearn.utils.validation import check_is_fitted
 
+from emulsify.base import BaseMixin
 
-class BaseLinear:
-    def set_engine(self, engine='sklearn', **kwargs):
-        self.engine = engine
-        # ToDo: add engine validation check
-        # ToDo: set up environment (H20, spark, and probably vowpall-wabbit)
-        self.model_kwargs = kwargs or {}
-        if engine == 'statsmodels':
-            self._statsmodels_engine()
-        else:
-            self._sklearn_engine()
 
-    def _statsmodels_engine(self):
-        raise ValueError("Not implemented for statsmodels API")
-
-    def _sklearn_engine(self):
-        raise ValueError("Not implemented for scikit-learn API")
-
+class BaseLinear(BaseMixin):
     def tidy(self):
         check_is_fitted(self, 'estimates_')
         tidy_df = pd.DataFrame({'term': self.names_, 'estimate': self.estimates_, 'std.error': self.std_errors_,
